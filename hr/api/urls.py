8cf348cda1e0a7222b192caa2eb2bbf78931e9b8
Utkarsh Gupta <utkarsh.gupta@anmaksolar.com>
2025-08-12 17:43:47 +0530

    v0.0.59

--- hr/api/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import EmployeeViewSet, AccessProfileViewSet
-
-router = DefaultRouter()
-router.register(r'employees', EmployeeViewSet, basename='hr-employees')
-router.register(r'access-profiles', AccessProfileViewSet, basename='hr-access-profiles')
-urlpatterns = router.urls
